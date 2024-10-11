2092fa5cf04e88016d8f584795ce28c05ba20d21
imtanmayraj <tanmaymth@gmail.com>
2024-10-11 09:29:16 +0530

    inbuilt files

---
+++ src/main/java/com/tanmay/App/Alien.java
@@ -0,0 +1,9 @@
+package com.tanmay.App;
+
+public class Alien {
+
+    public void code()
+    {
+        System.out.println("coding");
+    }
+}
---
+++ src/main/java/com/tanmay/App/Springbootdemo2Application.java
@@ -0,0 +1,23 @@
+package com.tanmay.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootdemo2Application.class, args);
+
+//		System.out.println("hello tanmay");
+
+//		Alien obj=new Alien();
+//		obj.code();
+
+		Alien obj;
+
+
+
+	}
+
+}
